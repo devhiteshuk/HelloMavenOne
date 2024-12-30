9642bc272801c11de11061bb442f5803cd28877e
Hiteshkumar Dhamshaniya <dev.hitesh.uk@gmail.com>
2024-12-30 08:27:34 +0000

    Comment some code for testing

--- src/main/scala/spark/BitcoinDataProcessing.scala
+++ src/main/scala/spark/BitcoinDataProcessing.scala
@@ -33,8 +33,8 @@ object BitcoinDataProcessing {
     println("*************************************************************************************")
 
     transformData(df)
-    calculateAveragePrices(df)
-    calculateMovingAverage(df)
+    //calculateAveragePrices(df)
+    //calculateMovingAverage(df)
     spark.stop()
 
   }
